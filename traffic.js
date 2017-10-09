0e16265e9ef5897d889ce23af63a9f51dd37240d
Your Name <you@example.com>
2017-10-09 15:52:42 -0400

    second commit

--- traffic.js
+++ traffic.js
@@ -9,24 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
   /********************************************************************
    SET TIME OUT METHOD FOR MANIPULATING WHEN THE LIGHTS WILL TURN ON***/
 
-    setTimeout(function(){ 
-      redLight.style.display = "block";
-    }, 500); 
-    setTimeout(function(){ 
-      redLight.style.display = "none";
-    }, 2500); 
+   setTimeout(function(){ 
+    greenLight.style.display = "block";
+    greenLight.style.marginTop = "140" + "px";
+  }, 500); 
+  setTimeout(function(){ 
+    greenLight.style.display = "none";
+  }, 2500); 
+  setTimeout(function(){
+    yellowLight.style.display = "block";
+    yellowLight.style.marginTop = "70" + "px";
+  }, 3000);
     setTimeout(function(){
-      yellowLight.style.display = "block";
-      yellowLight.style.marginTop = "70" + "px";
-    }, 3000);
-      setTimeout(function(){
-      yellowLight.style.display = "none";
-    }, 5500);
-      setTimeout(function(){
-      greenLight.style.display = "block";
-      greenLight.style.marginTop = "140" + "px";
-    }, 6000);
-      setTimeout(function(){
-      greenLight.style.display = "none";
-    }, 7500);
+    yellowLight.style.display = "none";
+  }, 5500);
+    setTimeout(function(){
+    redLight.style.display = "block";
+  }, 6000);
+    setTimeout(function(){
+    redLight.style.display = "none";
+  }, 9000);
 });
